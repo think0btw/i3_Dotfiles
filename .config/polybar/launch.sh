d6d948bcaff86dd957d2b1dc75c461997eaa05b4
Think0 <think0btw@gmail.com>
2026-01-08 19:02:25 +0000

    Update launch.sh

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# Kill running polybar instances
 killall -q polybar
-
-# Wait until processes are gone
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 # Launch bar
